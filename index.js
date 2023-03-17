a3c774a94fddd39dbaf74d09c62b10aa418814d2
Filippe Braga <filippeffx@hotmail.com>
2023-03-16 23:04:57 -0300

    morgan and slugify

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const { errorHandler, notFound } = require('./middlewares/errorHandler');
 const authRoute = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 dbConnect();
+
+app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
