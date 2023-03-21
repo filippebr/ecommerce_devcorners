1c85409a8a9d4d22f7be090979da34146bd2a42e
Filippe Braga <filippeffx@hotmail.com>
2023-03-20 23:56:43 -0300

    update blog

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -6,13 +6,22 @@ const validateMongoDbId = require('../utils/validateMongodbId');
 const createBlog = asyncHandler(async(req, res) => {
   try {
     const newBlog = await Blog.create(req.body);
-    res.json({
-      status: 'success',
-      newBlog, 
-    })
+    res.json(newBlog);
   } catch(err) {
     throw new Error(err);
   }
 });
 
-module.exports = { createBlog };
+const updateBlog = asyncHandler(async(req, res) => {
+  const { id } = req.params;
+  try {
+    const updateBlog = await Blog.findByIdAndUpdate(id, req.body, { 
+      new: true 
+    });
+    res.json(updateBlog);
+  } catch(err) {
+    throw new Error(err);
+  }
+});
+
+module.exports = { createBlog, updateBlog };
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createBlog } = require('../controller/blogCtrl');
+const { createBlog, updateBlog } = require('../controller/blogCtrl');
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.post('/', authMiddleware, isAdmin, createBlog)
+router.post('/', authMiddleware, isAdmin, createBlog);
+router.put('/:id', authMiddleware, isAdmin, updateBlog);
 
 module.exports = router;
