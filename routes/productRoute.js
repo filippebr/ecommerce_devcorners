8fefc44e1b9735169019daddee02e3ed67979719
Filippe Braga <filippeffx@hotmail.com>
2023-03-23 19:55:57 -0300

    wishlist controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,13 +4,15 @@ const {
   getProduct, 
   getAllProducts, 
   updateProduct, 
-  deleteProduct 
+  deleteProduct, 
+  addToWishList
 } = require('../controller/productCtrl');
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/', authMiddleware, isAdmin, createProduct);
 router.get('/products', getAllProducts);
+router.put('/wishlist', authMiddleware, addToWishList);
 router.put('/:id', authMiddleware, isAdmin, updateProduct);
 router.get('/:id', getProduct);
 router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
