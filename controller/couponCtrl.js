0f2cf1cdc57ee5b43507c62a9f95008837371114
Filippe Braga <filippeffx@hotmail.com>
2023-03-24 23:24:50 -0300

    coupon controller

---
+++ controller/couponCtrl.js
@@ -0,0 +1,13 @@
+const Coupon = require('../models/couponModel');
+const validateMongoDbId = require('../utils/validateMongoDbId');
+const asyncHandler = require('express-async-handler');
+
+const createCoupon = asyncHandler(async(req, res) => {
+  try {
+    
+  } catch (err) {
+
+  }
+});
+
+module.exports = {  };
