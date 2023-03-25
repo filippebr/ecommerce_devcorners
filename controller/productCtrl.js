a5626cb46fcd0748e5f76b0f243e9e94084f75d0
Filippe Braga <filippeffx@hotmail.com>
2023-03-24 21:26:54 -0300

    addtowishlist new version

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -102,40 +102,66 @@ const getAllProducts = asyncHandler(async (req, res) => {
   }
 }); 
 
-const addToWishList = asyncHandler(async(req, res) => {
+const addToWishList = asyncHandler(async (req, res) => {
   const { _id } = req.user;
   const { prodId } = req.body;
 
   try {
     const user = await User.findById(_id);
-    const alreadyAdded = user.wishlist.find((id) => id.toString() === prodId);
-    if (alreadyAdded) {
-      let user = await User.findByIdAndUpdate(
-        _id, 
-        {
-          $pull: { wishlist: prodId },
-        }, 
-        {
-          new: true
-        }
-      );
-      res.json(user);
-    } else {
-      let user = await User.findByIdAndUpdate(
-        _id, 
-        {
-          $push: { wishlist: prodId },
-        }, 
-        {
-          new: true
-        }
-      );
-      res.json(user);
-    }
-  } catch (err) {
-    throw new Error(err);
+
+    const isAlreadyAdded = user.wishlist.includes(prodId);
+
+    const update = isAlreadyAdded
+      ? { $pull: { wishlist: prodId } }
+      : { $push: { wishlist: prodId } };
+
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      update,
+      { new: true }
+    );
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
 });
+
+// const addToWishList = asyncHandler(async(req, res) => {
+//   const { _id } = req.user;
+//   const { prodId } = req.body;
+
+//   try {
+//     const user = await User.findById(_id);
+//     const alreadyAdded = user.wishlist.find((id) => id.toString() === prodId);
+//     if (alreadyAdded) {
+//       let user = await User.findByIdAndUpdate(
+//         _id, 
+//         {
+//           $pull: { wishlist: prodId },
+//         }, 
+//         {
+//           new: true
+//         }
+//       );
+//       res.json(user);
+//     } else {
+//       let user = await User.findByIdAndUpdate(
+//         _id, 
+//         {
+//           $push: { wishlist: prodId },
+//         }, 
+//         {
+//           new: true
+//         }
+//       );
+//       res.json(user);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// });
 
 // const rating = asyncHandler(async(req, res) => {
 //   const { _id } = req.user;
