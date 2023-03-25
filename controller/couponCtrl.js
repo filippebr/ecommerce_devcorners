ead88fbee41de458eef7ca7ba3b971b47c5b87f3
Filippe Braga <filippeffx@hotmail.com>
2023-03-24 23:29:51 -0300

    create coupon

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -4,10 +4,11 @@ const asyncHandler = require('express-async-handler');
 
 const createCoupon = asyncHandler(async(req, res) => {
   try {
-    
+    const newCoupon = await Coupon.create(req.body);
+    res.json(newCoupon);
   } catch (err) {
-
+    throw new Error(err);
   }
 });
 
-module.exports = {  };
+module.exports = { createCoupon };
