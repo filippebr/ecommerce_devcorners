e35fec958a2e969fe55f86302d657d4a0585a400
Filippe Braga <filippeffx@hotmail.com>
2023-03-25 17:26:37 -0300

    cloudinary file and env vars

---
+++ utils/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET
+});
+
+const cloudinaryUploadImg = async(fileToUploads) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUploads, (result) => {
+      resolve({
+          url: result.secure_url,
+        }, 
+        {
+          resource_type: 'auto'
+        }
+      );
+    });
+  });
+};
+
+module.exports = cloudinaryUploadImg;
