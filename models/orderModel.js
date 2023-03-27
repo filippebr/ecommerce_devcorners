4022bac6a4d636f973364e3975bfa07433a83b51
Filippe Braga <filippeffx@hotmail.com>
2023-03-26 23:55:53 -0300

    cart model

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,37 +1,40 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var orderSchema = new mongoose.Schema({
-  products: [
-    {
-      product: {
-        type: mongoose.ObjectId,
-        ref: "Product",
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
       },
-      count: Number,
-      color: String,
-    },
-  ],
-  paymentIntent: {},
-  orderStatus: {
-    type: String,
-    default: "Not Processed",
-    enum: [
-      "Not Processed", 
-      "Cash on Delivery", 
-      "Processing", 
-      "Dispatched", 
-      "Cancelled", 
-      "Delivered"
     ],
-  },
-  orderBy: {
-    type: mongoose.ObjectId,
-    ref: "Product", 
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed", 
+        "Cash on Delivery", 
+        "Processing", 
+        "Dispatched", 
+        "Cancelled", 
+        "Delivered"
+      ],
+    },
+    orderBy: {
+      type: mongoose.ObjectId,
+      ref: "Product", 
+    }
+  }, 
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true,
-});
+);
 
 //Export the model
 module.exports = mongoose.model('Order', orderSchema);
