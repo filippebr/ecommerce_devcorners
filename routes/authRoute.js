d7f4198d954fc4f90dba1f578a97d6a86b52247e
Filippe Braga <filippeffx@hotmail.com>
2023-03-30 13:11:44 -0300

    fixed routes for auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -39,24 +39,24 @@ router.put('/resetPassword/:token', resetPassword);
 
 router.put('/password', authMiddleware, updatePassword);
 router.post('/login', loginUser);
-router.post('/adminLogin', loginAdmin);
+router.post('/admin-login', loginAdmin);
 router.post('/cart', authMiddleware, userCart);
-router.post('/cart/applyCoupon', authMiddleware, applyCoupon);
-router.post('/cart/cashOrder', authMiddleware, createOrder);
+router.post('/cart/apply-coupon', authMiddleware, applyCoupon);
+router.post('/cart/cash-order', authMiddleware, createOrder);
 router.get('/users', getAllUsers);
-router.get('/getOrders', authMiddleware, getOrders);
+router.get('/get-orders', authMiddleware, getOrders);
 router.get('/refresh', handleRefreshToken);
 router.get('/logout', logout);
 router.get('/wishlist', authMiddleware, getWishlist);
 router.get('/cart', authMiddleware, getUserCart);
 
 router.get('/:id', authMiddleware, isAdmin, getUser);
-router.delete('/emptyCart', authMiddleware, emptyCart);
+router.delete('/empty-cart', authMiddleware, emptyCart);
 router.delete('/:id', deleteUser);
 
 router.put('/editUser', authMiddleware, updateUser);
-router.put('/saveAddress', authMiddleware, saveAddress);
-router.put('/blockUser/:id', authMiddleware, isAdmin, blockUser);
-router.put('/unblockUser/:id', authMiddleware, isAdmin, unblockUser);
+router.put('/save-address', authMiddleware, saveAddress);
+router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
+router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
 
 module.exports = router;
