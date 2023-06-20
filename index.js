349c1d82e144c53f8d20498f0def2b64d4932b89
Filippe Braga <filippeffx@hotmail.com>
2023-06-19 21:19:43 -0300

    organize some imports

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const { errorHandler, notFound } = require('./middlewares/errorHandler');
+
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const dbConnect = require('./config/dbConnect');
-const { errorHandler, notFound } = require('./middlewares/errorHandler');
+
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute');
@@ -12,8 +17,7 @@ const prodCategoryRouter = require('./routes/prodCategoryRoute');
 const blogCategoryRouter = require('./routes/blogCategoryRoute');
 const brandRouter = require('./routes/brandRoute');
 const couponRouter = require('./routes/couponRoute');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
+
 dbConnect();
 
 app.use(morgan('dev'));
